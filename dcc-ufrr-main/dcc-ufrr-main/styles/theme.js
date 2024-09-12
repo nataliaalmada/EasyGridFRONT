a83cda319e9375fe0a60f298f084f7452644b376
Natalia Almada <53869225+nataliaalmada@users.noreply.github.com>
2024-09-12 00:05:20 -0300

    Add files via upload

---
+++ dcc-ufrr-main/dcc-ufrr-main/styles/theme.js
@@ -0,0 +1,37 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        color: "gray.600",
+        lineHeight: "tall",
+        background: "url('/images/bg-block-gray.png')  #fff",
+      },
+      a: {
+        color: "teal.500",
+      },
+    },
+  },
+  fonts: {
+    body: `'Noto Sans', sans-serif`,
+    heading: `'Roboto Condensed', sans-serif`,
+  },
+  colors: {
+    brand: {
+      50: "#EDF2FC",
+      100: "#DCE6FA",
+      200: "#C9D9F7",
+      300: "#A6BFF2",
+      400: "#7099EB",
+      500: "#3B73E3",
+      600: "#143D8F",
+      700: "#143D8F",
+      800: "#0D2659",
+      900: "#081735",
+    },
+    linkColor: "#007AB8",
+  },
+});
+
+export default theme;
